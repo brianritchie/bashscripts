bb8ec2c675dde57b6f19bfa4f75070e38d400864
Brian Ritchie <esqbrianritchie@gmail.com>
2012-05-13 21:06:56 +0800

    Added in some additional packages
    Samba + Dropbox + Base Tools

--- kpcsb-fileserver.sh
+++ kpcsb-fileserver.sh
@@ -23,6 +23,14 @@ apt-get -y install aptitude
 aptitude -y full-upgrade
 
 ###########################################################
+# base tools
+###########################################################
+
+aptitude install vim htop git-core build-essential 
+aptitude linux-headers-3.2.0-23-generic
+aptitude install python-gpgme
+
+###########################################################
 # main poweruser
 ###########################################################
 
@@ -32,7 +40,13 @@ aptitude -y full-upgrade
 # installation of Samba
 ###########################################################
 
+aptitude install samba samba-common smbfs
+aptitude install python-glade2
+
+
 ###########################################################
 # installation of Dropbox
 ###########################################################
 
+wget http://dl-web.dropbox.com/u/17/dropbox-lnx.x86_64-1.4.3.tar.gz
+tar -xvzf dropbox-lnx.x86_64-1.4.3.tar.gz
