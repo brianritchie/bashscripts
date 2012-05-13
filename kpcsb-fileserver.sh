2fed406af9b2ac8fb6b4a9833d64ffd898ce3695
Brian Ritchie <esqbrianritchie@gmail.com>
2012-05-14 01:28:46 +0800

    Revert "Deleted old file"
    This reverts commit 7c00feff8271af472948c058488754ab431112e7.

---
+++ kpcsb-fileserver.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+###########################################################
+# hostname
+###########################################################
+
+echo "cerebrum" > /etc/hostname
+hostname -F /etc/hostname
+
+###########################################################
+# date and timezone
+###########################################################
+
+export TZ=Asia/Kuala_Lumpur
+date
+
+###########################################################
+# first update of system
+###########################################################
+
+apt-get update
+apt-get -y install aptitude
+aptitude -y full-upgrade
+
+###########################################################
+# base tools
+###########################################################
+
+aptitude -y install vim htop git-core build-essential 
+aptitude -y install linux-headers-3.2.0-23-generic
+aptitude -y install python-gpgme
+
+###########################################################
+# main poweruser
+###########################################################
+
+###########################################################
+# main script and download folder
+###########################################################
+
+mkdir ~/scripts
+cd ~/scripts
+
+###########################################################
+# downloading important packages
+###########################################################
+
+wget http://linux.dropbox.com/packages/dropbox.py
+wget http://humanityinbinary.com/VMwareTools-8.8.1-528969.tar.gz
+
+
+###########################################################
+# installation of Samba
+###########################################################
+
+aptitude -y install samba samba-common smbfs
+aptitude -y install python-glade2
+
+
+###########################################################
+# installation of Dropbox
+###########################################################
+
+wget http://dl-web.dropbox.com/u/17/dropbox-lnx.x86_64-1.4.3.tar.gz
+tar -xvzf dropbox-lnx.x86_64-1.4.3.tar.gz
