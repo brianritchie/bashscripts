8d952f19a821d86b40f2e7c52610d097a7627c1e
Brian Ritchie <esqbrianritchie@gmail.com>
2012-12-27 14:55:48 -0700

    added counter bash file

--- deletecounter.sh
+++ deletecounter.sh
@@ -4,9 +4,7 @@ x=10
 while [ $x -le 18 ]
 do
   echo "Deleting $x folder"
-  rm -rf Arrested.Development.S02E$x.DVDRip.XviD-SAPHiRE/*.r*
-  rm -rf Arrested.Development.S02E$x.DVDRip.XviD-SAPHiRE/*sample*.avi
-  rm -rf Arrested.Development.S02E$x.DVDRip.XviD-SAPHiRE/*subs
+  rm -rf filename.$x/*.r*
   x=$(( $x + 1 ))
 
 done
