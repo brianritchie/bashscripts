95677a584d529cf576a7bf283f77b3c9f11c0555
Brian Ritchie <sayhello@brianritchie.me>
2017-05-07 20:50:02 +0800

    Added Greed Setup Script

---
+++ greed.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+####This will setup the basics
+
+## Rclone - as per website
+
+# Fetch and Unpack
+
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+
+# Copy binary file
+
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+
+## Git
+
+sudo apt-get install git
+
+## TumblrDownloader
+
+git clone https://github.com/DiSiqueira/TumblrDownloader.git
+
+## MangaDownloader
+
+git clone https://github.com/jiaweihli/manga_downloader.git
+
+## Youtube-dl
+
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+## Aria2
+
+sudo apt-get install aria2
+
+## JDK
+
+sudo apt-get install default-jdk
+
+
+## RipMe
+
+wget https://github.com/4pr0n/ripme/releases/download/1.4.9/ripme.jar
